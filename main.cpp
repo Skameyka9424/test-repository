05ae152df1296e737e760a80d86a73ef95bab7aa
Skamuka <skameyka9424@gmail.com>
2025-11-26 20:02:23 +0500

    Создан каркас программы

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+vector<string> File_read() {
+    vector<string> file;
+    return file;
+}
+
+void Cout_file(vector<string> file) {
+
+}
+
+void File_write(vector<string> file) {
+
+}
+
 int main() {
-cout << "Hello world \n";
+vector<string> file = File_read();
+Cout_file(file);
+File_write(file);
 }
